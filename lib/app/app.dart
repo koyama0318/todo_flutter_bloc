3cfa985c412aa6469723a8a81bfc7729265df6e4
oka311119 <katoko.318@gmail.com>
2024-06-08 19:10:19 +0900

    feat: create todo list

---
+++ lib/app/app.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_todos/home/view/home_page.dart';
+import 'package:flutter_todos/theme/theme.dart';
+import 'package:todos_repository/todos_repository.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class App extends StatelessWidget {
+  const App({required this.todosRepository, super.key});
+
+  final TodosRepository todosRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider.value(
+      value: todosRepository,
+      child: const AppView(),
+    );
+  }
+}
+
+class AppView extends StatelessWidget {
+  const AppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: FlutterTodosTheme.light,
+      darkTheme: FlutterTodosTheme.dark,
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: const HomePage(),
+    );
+  }
+}
